5c416025608646249ec314ce9e31c247954e48f2
Estefano22 <sandulescustefano8@gmail.com>
2021-04-13 14:22:52 +0200

    hacer lo demas

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,52 @@
 package sample;
 
 
-
-
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 
 public class Controller {
+
+    @FXML
+    RadioButton radiobutton1;
+
+    @FXML
+    RadioButton radiobutton2;
+
+    @FXML
+    RadioButton radiobutton3;
+
+    @FXML
+    RadioButton radiobutton4;
+
+    @FXML
+    RadioButton radiobutton5;
+
+    @FXML
+    RadioButton radiobutton6;
+
+    @FXML
+    Label texto1;
+
+    @FXML
+    Label texto2;
+
+    @FXML
+    Label texto3;
+
+    @FXML
+    Label texto4;
+
+    @FXML
+    Label texto5;
+
+    @FXML
+    Label texto6;
+
+    @FXML
+    Label labelagregar;
+
+
+
+
 }
