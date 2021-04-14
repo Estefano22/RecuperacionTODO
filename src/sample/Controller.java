f61ec497cb1c2753afa6ceb7f77cfe1b52ac2ef9
estefano <sandulescustefano8@gmail.com>
2021-04-14 21:02:00 +0200

    hecho casi todo

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,8 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
 import java.util.ArrayList;
 
 public class Controller {
@@ -10,7 +12,6 @@ public class Controller {
     //Integer i=new Integer(2);
 
     ArrayList<String> lista = new ArrayList();
-
 
     @FXML
     public void initialize() {
@@ -62,32 +63,68 @@ public class Controller {
     @FXML
     Label labelagregar;
 
-public void tarea1(){
-    texto1.setText("");
+    @FXML
+    TextField textfieldagregar;
+
+
+    @FXML
+    public void tarea1(){
+        texto1.setText("");
+        labelagregar.setDisable(false);
+        activaragregartarea();
+        texto1.setText("");
 
     }
 
+    @FXML
     public void tarea2(){
         texto2.setText("");
+        labelagregar.setDisable(false);
+        activaragregartarea();
+        texto2.setText("");
+
     }
 
+    @FXML
     public void tarea3(){
         texto3.setText("");
+        labelagregar.setDisable(false);
+        activaragregartarea();
+        texto3.setText("");
+
     }
 
+    @FXML
     public void tarea4(){
         texto4.setText("");
+        labelagregar.setDisable(false);
+        activaragregartarea();
+        texto4.setText("");
+
     }
 
+    @FXML
     public void tarea5(){
         texto5.setText("");
+        labelagregar.setDisable(false);
+        activaragregartarea();
+        texto5.setText("");
+
     }
+
+    @FXML
     public void tarea6(){
         texto6.setText("");
+        labelagregar.setDisable(false);
+        activaragregartarea();
+        texto6.setText("");
+
     }
 
-
-
-
+public void activaragregartarea (){
+        if (! labelagregar. isDisabled()){
+            textfieldagregar.setDisable(false);
+        }
+}
 
 }
